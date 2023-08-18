3b3be748bc08942bbbafbd7e35e097c4ae5362b1
Muzadded <alifchowdhury27@gmail.com>
2023-08-19 02:29:28 +0600

    foodbee logo

--- blog.php
+++ blog.php
@@ -30,3 +30,27 @@
     <link href="css/custom.css" rel="stylesheet">
 
 </head>
+<body>
+
+<header class="header_in clearfix">
+	    <div class="container">
+	        <div id="logo">
+	            <a href="index.php">
+	                <img src="img/logo.png" width="140" height="35" alt="">
+	            </a>
+	        </div>
+	        <div class="layer"></div><!-- Opacity Mask Menu Mobile -->
+	        <ul id="top_menu">
+                <?php
+                    if (!empty($_SESSION['us_id'])) {
+                    }
+                    else{
+                    ?>
+            
+                <li><a href="#sign-in-dialog" id="sign-in" class="login">Sign In</a></li> <?php }?>
+                <li><a href="#" class="wishlist_bt_top" title="Your wishlist">Your wishlist</a></li>
+            </ul>
+
+
+            
+</body>
