5cd480bcaaad557e2d07be0ca04f54d746713dd8
Muzadded <alifchowdhury27@gmail.com>
2023-08-20 03:09:39 +0600

    edit_blog.php header

--- edit_blog.php
+++ edit_blog.php
@@ -23,3 +23,115 @@
 
       $bg_id=intval($_GET['fid']);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="FoodBEE - Quality delivery or takeaway food">
+    <meta name="author" content="Ansonika">
+    <title>FoodBEE - Discover & Book the best restaurants at the best price</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img2/favicon.ico" type="image2/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img2/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img2/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img2/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img2/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap_customized.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- SPECIFIC CSS -->
+    <link href="css/review.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+    
+<header class="header_in clearfix">
+	    <div class="container">
+	        <div id="logo">
+	            <a href="index.php">
+	                <img src="img/logo.png" width="140" height="35" alt="">
+	            </a>
+	        </div>
+	        <div class="layer"></div><!-- Opacity Mask Menu Mobile -->
+	        <ul id="top_menu">
+                <?php
+                    if (!empty($_SESSION['us_id'])) {
+                    }
+                    else{
+                    ?>
+            
+                <li><a href="#sign-in-dialog" id="sign-in" class="login">Sign In</a></li> <?php }?>
+                <li><a href="#" class="wishlist_bt_top" title="Your wishlist">Your wishlist</a></li>
+            </ul>
+            
+            <!-- /top_menu -->
+            <a href="#0" class="open_close">
+                <i class="icon_menu"></i><span>Menu</span>
+            </a>
+            <nav class="main-menu">
+                <div id="header_menu">
+                    <a href="#0" class="open_close">
+                        <i class="icon_close"></i><span>Menu</span>
+                    </a>
+                    <a href="index.php"><img src="img/logo.png" width="162" height="35" alt=""></a>
+                </div>
+                <ul>
+                    <li >
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li >
+                        <a href="restaurant.php">Restaurant</a>
+                    </li>
+                    <li >
+                        <a >Meal Planning</a>
+                    </li>
+                    <li >
+                        <a href="blog.php">Blog</a>
+                    </li>
+                    <li >
+                        <a href="recipes.php">Recipes</a>
+                    </li>
+                    
+                    <?php
+                    if (!empty($_SESSION['us_id'])) {
+                        $cid = $_SESSION['us_id'];
+                        
+                    
+                    $query=mysqli_query($connect, "SELECT * from user where us_id = $cid");
+                    $row=mysqli_fetch_assoc($query); 
+                    
+                    ?>
+                    <li class="submenu">
+                        &nbsp;&nbsp;<img src="img2/<?php echo htmlentities($row['us_pic']); ?>" style="display: inline-block; border-radius: 50%; width: 30px; height: 30px;" alt="Image"> 
+                        <a style="display: inline-block; margin-left: 10px;"><?php echo htmlentities($row['us_name']); ?></a> 
+                        <ul>
+                            <li><a href="#0">Add Restaurant</a></li>
+                            <li><a href="?logout=1" style="color: red;">Log Out</a></li>
+                                                         
+                        </ul>
+                    </li>
+                    <?php 
+                        }
+                    else{}?>
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- /header -->
+</body>
