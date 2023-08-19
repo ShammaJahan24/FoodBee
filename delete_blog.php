b264f8bb98250f5e1ed0c03e6b91a16e644036a8
Muzadded <alifchowdhury27@gmail.com>
2023-08-20 00:13:56 +0600

    dlt_blog done

--- delete_blog.php
+++ delete_blog.php
@@ -20,3 +20,36 @@
     or die("Can not connect");
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Deletion</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="ui text segment container" style="margin-top: 20vh;">
+		<?php
+
+			$bg_id = $_GET["fid"];
+
+			mysqli_query( $connect, "DELETE FROM blog_post WHERE bg_id=$bg_id" )
+
+				or die("Can not execute query");
+
+
+
+			//echo "<h2>Record deleted</h2><br>";
+
+
+		     header("Location: my_blog.php");
+
+			//echo "<div><a href=commentOn.php><button class='ui right floated blue button'>OK</button></a></div>";
+
+		?>
+	</div>
+</body>
+</html>
