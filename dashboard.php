2e6d52930ff408cee0e47a5668c5b575fe7c038b
Osman Goni <ogoni202046@bscse.uiu.ac.bd>
2023-08-19 23:34:55 +0600

    2nd commit

--- dashboard.php
+++ dashboard.php
@@ -173,3 +173,300 @@
 					<div class="col-xl-3 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
 						<div class="widget-stat card">
 							<div class="card-body p-4">
+								<div class="media ai-icon">
+									<span class="mr-3 bgl-primary text-primary">
+										<!-- <i class="ti-user"></i> -->
+										<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-wallet" viewBox="0 0 16 16">
+										<path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z"/>
+										</svg>
+									</span>
+									<div class="media-body">
+										<h3 class="mb-0 text-black"><span class="counter ml-0">2000</span>৳</h3>
+										<p class="mb-0">Total Revenue</p>
+										<small>4% (30 days)</small>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
+						<div class="widget-stat card">
+							<div class="card-body p-4">
+								<div class="media ai-icon">
+									<span class="mr-3 bgl-primary text-primary">
+										<!-- <i class="ti-user"></i> -->
+										<svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 30.5H22.75C23.7442 30.4989 24.6974 30.1035 25.4004 29.4004C26.1035 28.6974 26.4989 27.7442 26.5 26.75V16.75C26.5 16.4185 26.3683 16.1005 26.1339 15.8661C25.8995 15.6317 25.5815 15.5 25.25 15.5C24.9185 15.5 24.6005 15.6317 24.3661 15.8661C24.1317 16.1005 24 16.4185 24 16.75V26.75C23.9997 27.0814 23.8679 27.3992 23.6336 27.6336C23.3992 27.8679 23.0814 27.9997 22.75 28H4C3.66857 27.9997 3.3508 27.8679 3.11645 27.6336C2.88209 27.3992 2.7503 27.0814 2.75 26.75V9.25C2.7503 8.91857 2.88209 8.6008 3.11645 8.36645C3.3508 8.13209 3.66857 8.0003 4 8H15.25C15.5815 8 15.8995 7.8683 16.1339 7.63388C16.3683 7.39946 16.5 7.08152 16.5 6.75C16.5 6.41848 16.3683 6.10054 16.1339 5.86612C15.8995 5.6317 15.5815 5.5 15.25 5.5H4C3.00577 5.50109 2.05258 5.89653 1.34956 6.59956C0.646531 7.30258 0.251092 8.25577 0.25 9.25V26.75C0.251092 27.7442 0.646531 28.6974 1.34956 29.4004C2.05258 30.1035 3.00577 30.4989 4 30.5Z" fill="#2F4CDD"/><path d="M25.25 0.5C24.0139 0.5 22.8055 0.866556 21.7777 1.55331C20.7499 2.24007 19.9488 3.21619 19.4758 4.35823C19.0027 5.50027 18.8789 6.75693 19.1201 7.96931C19.3613 9.1817 19.9565 10.2953 20.8306 11.1694C21.7047 12.0435 22.8183 12.6388 24.0307 12.8799C25.2431 13.1211 26.4997 12.9973 27.6418 12.5242C28.7838 12.0512 29.7599 11.2501 30.4467 10.2223C31.1334 9.19451 31.5 7.98613 31.5 6.75C31.498 5.093 30.8389 3.50442 29.6673 2.33274C28.4956 1.16106 26.907 0.501952 25.25 0.5ZM25.25 10.5C24.5083 10.5 23.7833 10.2801 23.1666 9.86801C22.5499 9.45596 22.0693 8.87029 21.7855 8.18506C21.5016 7.49984 21.4274 6.74584 21.5721 6.01841C21.7167 5.29098 22.0739 4.6228 22.5983 4.09835C23.1228 3.5739 23.791 3.21675 24.5184 3.07206C25.2458 2.92736 25.9998 3.00162 26.6851 3.28545C27.3703 3.56928 27.9559 4.04993 28.368 4.66661C28.7801 5.2833 29 6.00832 29 6.75C28.9989 7.74423 28.6035 8.69742 27.9004 9.40044C27.1974 10.1035 26.2442 10.4989 25.25 10.5Z" fill="#2F4CDD"/><path d="M6.5 13H12.75C13.0815 13 13.3995 12.8683 13.6339 12.6339C13.8683 12.3995 14 12.0815 14 11.75C14 11.4185 13.8683 11.1005 13.6339 10.8661C13.3995 10.6317 13.0815 10.5 12.75 10.5H6.5C6.16848 10.5 5.85054 10.6317 5.61612 10.8661C5.3817 11.1005 5.25 11.4185 5.25 11.75C5.25 12.0815 5.3817 12.3995 5.61612 12.6339C5.85054 12.8683 6.16848 13 6.5 13Z" fill="#2F4CDD"/><path d="M5.25 16.75C5.25 17.0815 5.3817 17.3995 5.61612 17.6339C5.85054 17.8683 6.16848 18 6.5 18H17.75C18.0815 18 18.3995 17.8683 18.6339 17.6339C18.8683 17.3995 19 17.0815 19 16.75C19 16.4185 18.8683 16.1005 18.6339 15.8661C18.3995 15.6317 18.0815 15.5 17.75 15.5H6.5C6.16848 15.5 5.85054 15.6317 5.61612 15.8661C5.3817 16.1005 5.25 16.4185 5.25 16.75Z" fill="#2F4CDD"/></svg>
+									</span>
+									<div class="media-body">
+										<h3 class="mb-0 text-black"><span class="counter ml-0">100</span></h3>
+										<p class="mb-0">Total Orders</p>
+										<small>4% (30 days)</small>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12">
+						<div class="card">
+							<div class="card-header border-0 pb-0 d-sm-flex d-block">
+								<div>
+									<h4 class="card-title mb-1">Orders Summary</h4>
+									<small class="mb-0"></small>
+								</div>
+								<div class="card-action card-tabs mt-3 mt-sm-0">
+									<ul class="nav nav-tabs" role="tablist">
+										<li class="nav-item">
+											<a class="nav-link active" data-toggle="tab" href="#user" role="tab">
+												Monthly
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" data-toggle="tab" href="#bounce" role="tab">
+												Weekly
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" data-toggle="tab" href="#session-duration" role="tab">
+												Today
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="card-body orders-summary">
+								<div class="d-flex order-manage p-3 align-items-center mb-4">
+									<a href="javascript:void(0);" class="btn fs-22 py-1 btn-success px-4 mr-3">25</a>
+									<h4 class="mb-0">New Orders <i class="fa fa-circle text-success ml-1 fs-15"></i></h4>
+									<a href="javascript:void(0);" class="ml-auto text-primary font-w500">Manage orders <i class="ti-angle-right ml-1"></i></a>
+								</div>
+								<div class="row">
+									<div class="col-sm-4 mb-4">
+										<div class="border px-3 py-3 rounded-xl">
+											<h2 class="fs-32 font-w600 counter">25</h2>
+											<p class="fs-16 mb-0">On Delivery</p>
+										</div>
+									</div>
+									<div class="col-sm-4 mb-4">
+										<div class="border px-3 py-3 rounded-xl">
+											<h2 class="fs-32 font-w600 counter">60</h2>
+											<p class="fs-16 mb-0">Delivered</p>
+										</div>
+									</div>
+									<div class="col-sm-4 mb-4">
+										<div class="border px-3 py-3 rounded-xl">
+											<h2 class="fs-32 font-w600 counter">7</h2>
+											<p class="fs-16 mb-0">Canceled</p>
+										</div>
+									</div>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="card">
+							<div class="card-header border-0 pb-0 d-sm-flex d-block">
+								<div>
+									<h4 class="card-title mb-1">Revenue</h4>
+									<small class="mb-0"></small>
+								</div>
+								<div class="dropdown mt-3 mt-sm-0">
+									<button type="button" class="btn btn-primary dropdown-toggle light fs-14" data-toggle="dropdown" aria-expanded="false">
+										Weekly
+									</button>
+									<div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Daily</a>
+										<a class="dropdown-item" href="#">Weekly</a>
+										<a class="dropdown-item" href="#">Monthly</a>
+									</div>
+								</div>
+							</div>
+							<div class="card-body revenue-chart px-3">
+									<div class="d-flex align-items-end pr-3 pull-right revenue-chart-bar">
+										<div class="d-flex align-items-end mr-4">
+											<img src="images/svg/ic_stat2.svg" height="22" width="22" class="mr-2 mb-1" alt=""/>
+											<div>
+												<small class="text-dark fs-14">Income</small>
+												<h3 class="font-w600 mb-0">৳<span class="counter">41,512</span>k</h3>
+											</div>
+										</div>
+										
+									</div>
+								<div id="chartBar"></div>
+							</div>
+						</div>
+					</div>
+					
+				 </div>
+            </div>
+        </div>
+	<!-- /main -->
+
+	<footer>
+        <div class="wave footer"></div>
+        <div class="container margin_60_40 fix_mobile">
+
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <h3 data-target="#collapse_1">Quick Links</h3>
+                    <div class="collapse dont-collapse-sm links" id="collapse_1">
+                        <ul>
+                            <li><a href="about.html">About us</a></li>
+                            <li><a href="submit-restaurant.php">Add your restaurant</a></li>
+                            <li><a href="help.html">Help</a></li>
+                            <li><a href="register.php">My account</a></li>
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="contacts.html">Contacts</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3 data-target="#collapse_2">Categories</h3>
+                    <div class="collapse dont-collapse-sm links" id="collapse_2">
+                        <ul>
+                            <li><a href="restaurant.php">Top Categories</a></li>
+                            <li><a href="grid-listing-filterscol-full-masonry.html">Best Rated</a></li>
+                            <li><a href="grid-listing-filterscol-full-width.html">Best Price</a></li>
+                            <li><a href="grid-listing-filterscol-full-masonry.html">Latest Submissions</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                        <h3 data-target="#collapse_3">Contacts</h3>
+                    <div class="collapse dont-collapse-sm contacts" id="collapse_3">
+                        <ul>
+                            <li><i class="icon_house_alt"></i>Mirpur 14 <br>Dhaka 1206</li>
+                            <li><i class="icon_mobile"></i>016628245488</li>
+                            <li><i class="icon_mail_alt"></i><a href="#0">admin@gmail.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                        <h3 data-target="#collapse_4">Keep in touch</h3>
+                    <div class="collapse dont-collapse-sm" id="collapse_4">
+                        <div id="newsletter">
+                            <div id="message-newsletter"></div>
+                            <form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
+                                <div class="form-group">
+                                    <input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+                                    <button type="submit" id="submit-newsletter"><i class="arrow_carrot-right"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="follow_us">
+                            <h5>Follow Us</h5>
+                            <ul>
+                                <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/twitter_icon.svg" alt="" class="lazy"></a></li>
+                                <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/facebook_icon.svg" alt="" class="lazy"></a></li>
+                                <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/instagram_icon.svg" alt="" class="lazy"></a></li>
+                                <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/youtube_icon.svg" alt="" class="lazy"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /row-->
+            <hr>
+            <div class="row add_bottom_25">
+                <div class="col-lg-6">
+                    <ul class="footer-selector clearfix">
+					    <li>
+                            <div class="styled-select lang-selector">
+                                <select>
+                                    <option value="English" selected>English</option>
+                                    
+                                    <option value="Bangla">Bangla</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="styled-select currency-selector">
+                                <select>
+                                    <option value="TK" selected>TK</option>
+                                    <option value="US Dollars">US Dollars</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/cards_all.svg" alt="" width="230" height="35" class="lazy"></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="additional_links">
+                        <li><a href="#0">Terms and conditions</a></li>
+                        <li><a href="#0">Privacy</a></li>
+                        <li><span>© FoodBEE</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/footer-->
+
+	<div id="toTop"></div><!-- Back to top button -->
+	
+	<!-- Sign In Modal -->
+<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
+    <div class="modal_header">
+        <h3>Sign In</h3>
+    </div>
+    <form method="GET" action=login.php >
+        <div class="sign-in-wrapper">
+            <a href="#0" class="social_bt facebook">Login with Facebook</a>
+            <a href="#0" class="social_bt google">Login with Google</a>
+            <div class="divider"><span>Or</span></div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" id="email">
+                <i class="icon_mail_alt"></i>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" name="password" id="password" value="">
+                <i class="icon_lock_alt"></i>
+            </div>
+            <div class="clearfix add_bottom_15">
+                <div class="checkboxes float-left">
+                    <label class="container_check">Remember me
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="float-right"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
+            </div>
+            <div class="text-center">
+                <input type="submit" value="Log In" class="btn_1 full-width mb_5">
+                Don’t have an account? <a href="register.php">Sign up</a>
+            </div>
+            <div id="forgot_pw">
+                <div class="form-group">
+                    <label>Please confirm login email below</label>
+                    <input type="email" class="form-control" name="email_forgot" id="email_forgot">
+                    <i class="icon_mail_alt"></i>
+                </div>
+                <p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
+                <div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
+            </div>
+        </div>
+    </form>
+    <!--form -->
+</div>
+
+
+<!-- /Sign In Modal -->
+	
+	<!-- COMMON SCRIPTS -->
+    <script src="js/common_scripts.min.js"></script>
+    <script src="js/common_func.js"></script>
+    <script src="assets/validate.js"></script>
+
+    <!-- SPECIFIC SCRIPTS -->
+    <script src="js/sticky_sidebar.min.js"></script>
+    <script src="js/specific_listing.js"></script>
+
+    <!-- Map -->
+    <script src="js/main_map_scripts.js"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap"></script>
+
+</body>
+</html>
