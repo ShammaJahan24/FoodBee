dcf9c72d5085087092cc6fb1e1dbe294a991fb76
Muzadded <alifchowdhury27@gmail.com>
2023-08-20 01:18:59 +0600

    blog-post.php header

--- blog-post.php
+++ blog-post.php
@@ -51,3 +51,81 @@
     <link href="css/custom.css" rel="stylesheet">
 
 </head>
+
+<body>
+<header class="header_in clearfix">
+	    <div class="container">
+	        <div id="logo">
+	            <a href="index.php">
+	                <img src="img/logo.png" width="140" height="35" alt="">
+	            </a>
+	        </div>
+	        <div class="layer"></div><!-- Opacity Mask Menu Mobile -->
+	        <ul id="top_menu">
+                <?php
+                    if (!empty($_SESSION['us_id'])) {
+                    }
+                    else{
+                    ?>
+            
+                <li><a href="#sign-in-dialog" id="sign-in" class="login">Sign In</a></li> <?php }?>
+                <li><a href="#" class="wishlist_bt_top" title="Your wishlist">Your wishlist</a></li>
+            </ul>
+            
+            <!-- /top_menu -->
+            <a href="#0" class="open_close">
+                <i class="icon_menu"></i><span>Menu</span>
+            </a>
+            <nav class="main-menu">
+                <div id="header_menu">
+                    <a href="#0" class="open_close">
+                        <i class="icon_close"></i><span>Menu</span>
+                    </a>
+                    <a href="index.php"><img src="img/logo.png" width="162" height="35" alt=""></a>
+                </div>
+                <ul>
+                    <li >
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li >
+                        <a href="restaurant.php">Restaurant</a>
+                    </li>
+                    <li >
+                        <a >Meal Planning</a>
+                    </li>
+                    <li >
+                        <a href="blog.php">Blog</a>
+                    </li>
+                    <li >
+                        <a href="recipes.php">Recipes</a>
+                    </li>
+                    
+                    <?php
+                    if (!empty($_SESSION['us_id'])) {
+                        $cid = $_SESSION['us_id'];
+                        
+                    
+                    $query=mysqli_query($connect, "SELECT * from user where us_id = $cid");
+                    $row=mysqli_fetch_assoc($query); 
+                    
+                    ?>
+                    <li class="submenu">
+                        &nbsp;&nbsp;<img src="img2/<?php echo htmlentities($row['us_pic']); ?>" style="display: inline-block; border-radius: 50%; width: 30px; height: 30px;" alt="Image"> 
+                        <a style="display: inline-block; margin-left: 10px;"><?php echo htmlentities($row['us_name']); ?></a> 
+                        <ul>
+                            <li><a href="#0">Add Restaurant</a></li>
+                            <li><a href="?logout=1" style="color: red;">Log Out</a></li>
+                                                         
+                        </ul>
+                    </li>
+                    <?php 
+                        }
+                    else{}?>
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- /header -->
+</body>
