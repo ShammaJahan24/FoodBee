9166918b855ccb2a966831db0bdb30b1fa8ec462
Muzadded <alifchowdhury27@gmail.com>
2023-08-19 20:20:58 +0600

    blog_entry.php db connection

---
+++ blog_entry.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    if(isset($_GET['logout'])) {
+        session_unset();
+        session_destroy();    
+        header("Location: restaurant.php");
+        exit;
+        } 
+    include 'db_connect.php';
+    $connect = mysqli_connect(HOST, USER, PASS, DB)
+        or die("Can not connect");
+
+	if (!empty($_SESSION['us_id'])) {
+        $cid = $_SESSION['us_id'];
+      }
+      else{}
+?>
