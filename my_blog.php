f90d49d796c6d117d78ee72681053c154b5a4ed2
Muzadded <alifchowdhury27@gmail.com>
2023-08-19 21:22:03 +0600

    my_blog.php db connect

--- my_blog.php
+++ my_blog.php
@@ -1,3 +1,26 @@
+<?php
+  session_start();
+  if(isset($_GET['logout'])) {
+    session_unset();
+    session_destroy();
+
+    // clear authentication cookies or tokens here
+    // ...
+
+    header("Location: blog.php");
+    exit;
+    } 
+  if (!empty($_SESSION['us_id'])) {
+    $cid = $_SESSION['us_id'];
+    
+  }
+  else{}
+  include 'db_connect.php';
+  $connect = mysqli_connect(HOST, USER, PASS, DB)
+    or die("Can not connect");
+
+  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
